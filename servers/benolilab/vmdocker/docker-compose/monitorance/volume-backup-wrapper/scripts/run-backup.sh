60e36b90a84baab6b657df74544a60ee146e72eb
Paul Krimm <pm.krimm@gmail.com>
2025-06-24 17:18:11 -0400

    Added docker stop label to services in compose files so that offen backup service will handle stopping services during backup to avoid long delays while backups are being uploaded - removed steps to start/stop services in volume-backup-wrapper scripts

--- servers/benolilab/vmdocker/docker-compose/monitorance/volume-backup-wrapper/scripts/run-backup.sh
+++ servers/benolilab/vmdocker/docker-compose/monitorance/volume-backup-wrapper/scripts/run-backup.sh
@@ -15,8 +15,8 @@ SCRIPTPATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 "$SCRIPTPATH/lock.sh"
 "$SCRIPTPATH/wait-for-quiet.sh"
-"$SCRIPTPATH/service-toggle.sh" stop 3
+#"$SCRIPTPATH/service-toggle.sh" stop 3
 "$SCRIPTPATH/backup-now.sh"
-"$SCRIPTPATH/service-toggle.sh" start 5
+#"$SCRIPTPATH/service-toggle.sh" start 5
 "$SCRIPTPATH/unlock.sh"
 "$SCRIPTPATH/preview-schedule.sh"
